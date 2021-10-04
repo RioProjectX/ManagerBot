ce097a30b43e80ce7ca14118aaa1f58c6d345f3c
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-10-04 18:59:21 +0700

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -366,7 +366,7 @@ def AnkiVector_about_callback(update, context):
     if query.data == "aboutmanu_":
         query.message.edit_text(
             text=f"* Hi There  The name's {dispatcher.bot.first_name} \n\nAs  You I'm a next generational group management bot developed by Anki Vector Updates.* "
-            f"\n\n Join [RioUpdates](https://t.me/riobotsupport) To Keep Yourself Updated About {dispatcher.bot.first_name}"
+            f"\n\n Join [RioUpdates](https://t.me/RioGroupSupport) To Keep Yourself Updated About {dispatcher.bot.first_name}"
             f"\n\n I have the normal GROUP MANAGING functions like flood control, a warning system etc but I mainly have the advanced and handy Antispam system and the SIBYL banning system which safegaurds and helps your group from spammers."
             f"\n\nI Can Manage Your Groups Smoothly, With Some Special Features"
             f"\n\nYou Can Know More About Me By Clicking The Below Buttons",
